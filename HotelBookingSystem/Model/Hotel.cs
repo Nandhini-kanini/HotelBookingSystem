63af6783ff824fa9d55caf92361b010a8ec60868
Nandhini R <nandhini.kanini@gmail.com>
2023-05-26 11:32:06 +0530

    Initial commit

---
+++ HotelBookingSystem/Model/Hotel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingSystem.Model
+{
+    public class Hotel
+    {
+        [Key]
+        public int HotelId { get; set; }
+        public string ?HotelName { get; set; }
+        public string ?HotelAddress { get; set; }
+        public string ?HotelCity { get; set; }
+        public string ?HotelCountry { get; set; }
+        public ICollection<Room>? Rooms { get; set; }
+
+        public ICollection<Customer>? Customers { get; set; }
+    }
+}
---
+++ HotelBookingSystem/Model/HotelContext.cs
@@ -0,0 +1,26 @@
+﻿using HotelBookingSystem.Model;
+using HotelManagement.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingSystem.Model
+{
+    public class HotelContext : DbContext
+    {
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        
+
+        public HotelContext(DbContextOptions<HotelContext> options) : base(options)
+        {
+
+        }
+    }
+}
+
+
+   
+
